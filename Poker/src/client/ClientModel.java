aaa56b5b0a29a55381a99bea418c6207a2982edb
Alekseylv <alekseylv@me.com>
2013-07-16 15:02:40 +0300

    Implemented ClientModel

--- Poker/src/client/ClientModel.java
+++ Poker/src/client/ClientModel.java
@@ -1,8 +1,63 @@
 package client;
 
-public class ClientModel {
+import java.util.Observable;
 
+import poker.arturka.Card;
+
+public class ClientModel extends Observable {
+
+	private Card mycards[];
+	private Card fieldcards[];
+	private state state;
+	private int cash;
+	
+	
 	public ClientModel() {
-		
+		this.mycards = new Card[2];
+		this.fieldcards = new Card[5];
+		this.state = state.READY;
 	}
+	
+	public void setCash(int newCash) {
+		this.cash = newCash;
+		
+		setChanged();
+        notifyObservers(mycards);
+	}
+	
+	public int getCash() {
+		return this.cash; 
+	}
+	
+	public Card[] getMyCards() {
+		return mycards.clone();
+	}
+	
+	public void setMyField(Card card1, Card card2) {
+		mycards[1] = card1;
+		mycards[2] = card2;
+		
+		setChanged();
+        notifyObservers(mycards);
+	}
+	
+	public void changeFieldCards(Card newField[]) {
+		assert(newField.length == 5);
+		this.fieldcards = newField;
+		
+		setChanged();
+        notifyObservers(fieldcards);
+	}
+	
+	public state getState() {
+		return this.state;
+	}
+	
+	public void changeState(state newState) {
+		this.state = newState;
+		setChanged();
+        notifyObservers(state);
+	}
+	
+	
 }
