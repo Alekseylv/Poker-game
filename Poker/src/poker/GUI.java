8caf601e551f7b0ed13bd113e11fc63dded53872
Alekseylv <alekseylv@me.com>
2013-07-16 09:26:28 +0300

    Some comments

--- Poker/src/poker/GUI.java
+++ Poker/src/poker/GUI.java
@@ -1,5 +1,12 @@
 package poker;
 
+/**
+ * GUI is a class for managing user interfaces in
+ * a client. Used as an instance variable of Client class.
+ * 
+ *@author ???
+ */
+
 public class GUI {
 
 }
