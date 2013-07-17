8f5db24cc56abd49b03c55347f7173b24941b00e
Maksim <maksim.venediktov@gmail.com>
2013-07-17 09:55:15 +0300

    images were added

--- Poker/src/poker/GUI/LoginGUI.java
+++ Poker/src/poker/GUI/LoginGUI.java
@@ -31,12 +31,10 @@ public class LoginGUI extends JFrame implements ActionListener{
         labelName.setText("Your name: ");
         return labelName;
     }
-
     private JTextField textName(){
         textName.setBounds(100, 15, 120, 25);
         return textName;
     }
-
     private JButton buttonConnect(){
         JButton buttonConnect = new JButton();
 
@@ -46,21 +44,19 @@ public class LoginGUI extends JFrame implements ActionListener{
         return buttonConnect;
     }
 
-    @Override
     public void actionPerformed(ActionEvent buttonConnect) {
         PlayerName = textName.getText();
-
         showTable();
         this.dispose();
     }
 
     public static TableGUI showTable(){
-
         tableWindow.setLocation(screenSize.width / 2 - tableWindow.getSize().width / 2, screenSize.height / 2 - tableWindow.getSize().height / 2);
         tableWindow.setResizable(false);
         tableWindow.setVisible(true);
         tableWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         tableWindow.displayNick().setText(PlayerName);
+
         return tableWindow;
     }
 
