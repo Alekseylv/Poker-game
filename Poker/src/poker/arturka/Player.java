8a417c4ac5f1e14e63c89bb58f14029fa2d1e0ac
mordavolt <mordavolt@gmail.com>
2013-07-17 15:22:48 +0300

    ffs some new stuff

--- Poker/src/poker/arturka/Player.java
+++ Poker/src/poker/arturka/Player.java
@@ -15,6 +15,7 @@ public class Player extends Observable {
     private boolean dealer;
     private boolean fold;
     private int bet;
+    private boolean inGame;
 
     public Player(int id){
         this.id=id;
@@ -65,18 +66,19 @@ public class Player extends Observable {
     public void toggleDealer() {
         dealer=!dealer;
     }
-    
     public void toggleFold() {
         fold=!fold;
     }
-    
-    public Card[] getHand() {
-    	Card[] tempHand = new Card[hand.length];
-    	for (int i = 0; i < tempHand.length; i++) {
-    		tempHand[i] = hand[i];
-    	}
-    	return tempHand;
+
+    public void setBet(int bet) {
+        this.bet = bet;
     }
-    
-    
+
+    public boolean isInGame() {
+        return inGame;
+    }
+
+    public void toggleInGame() {
+        inGame=!inGame;
+    }
 }
