8e6b88d8effef14efd7d05c29eee466bdc6c2042
Alekseylv <alekseylv@me.com>
2013-07-18 14:38:06 +0300

    TaskQueue javadocs

--- Poker/src/client/TaskQueue.java
+++ Poker/src/client/TaskQueue.java
@@ -5,19 +5,44 @@ import java.util.List;
 
 import commands.Command;
 
+/**
+ * Storage for commands received from server
+ * @author Aleksey
+ *
+ */
+
 public class TaskQueue {
 	
+	/**
+	 * taskList: data structure used for storing commands
+	 */
 	private List<Command> taskList;
 	
+	/**
+	 * Constructs and empty data structure
+	 */
 	public TaskQueue(){
 		taskList = new ArrayList <Command>();	
 	}
+	
+	/**
+	 * Synchronously adds a command to the taskList
+	 * and notifies {@link ClientGame}
+	 * @param task
+	 *  Command to add
+	 */
 
 	public synchronized void addTask(Command task){	
 		
 		taskList.add(task);		
 		notify();
 	}
+	
+	/**
+	 * Synchronously get a command from the taskList
+	 * @return
+	 * last command sent by the server
+	 */
 	
 	public  synchronized Command getNextTask(){
 		if(taskList.isEmpty()){
@@ -26,6 +51,13 @@ public class TaskQueue {
 			return taskList.remove(0);
 		}
 	}
+	
+	/**
+	 * Check's if the taskList is empty, 
+	 * wARNING this method is not synchronized
+	 * 
+	 * @return true if it's empty, false otherwise
+	 */
 	
 	public boolean isEmpty(){
 		if(taskList.size() == 0){
