924bb784c8472df4aeb00090d0362f29a6780c03
Alekseylv <alekseylv@me.com>
2013-07-18 14:25:40 +0300

    Connection javadocs

--- Poker/src/client/Conn.java
+++ Poker/src/client/Conn.java
@@ -7,11 +7,31 @@ import java.net.Socket;
 
 import message.data.ClientResponse;
 
+/**
+ * Represents the socket connection with the server
+ * @author Aleksey
+ *
+ */
+
 public class Conn {
 	
+	/**
+	 * socket: open socket with the server
+	 * out: outputstream of this socket
+	 */
 	public final Socket socket;
 	public final ObjectOutputStream out;
 	
+	
+	/**
+	 * Constructs a connection object using socket and outputstream
+	 * @param socket
+	 *  The open socket
+	 * @param out
+	 *  The outputstream
+	 * @throws IOException
+	 *  Unable to initialize or flush outputstream properly
+	 */
 	public Conn(Socket socket, OutputStream out) throws IOException {
 		this.socket = socket;
 		this.out =  new ObjectOutputStream(out);
@@ -19,7 +39,11 @@ public class Conn {
 		
 	}
 	
-	
+	/**
+	 * Sends server a message from the client through the outputstream
+	 * @param resp
+	 *  The message to be sent
+	 */
 	public void sendResponse(ClientResponse resp) {
 		try {
 			out.writeObject(resp);
