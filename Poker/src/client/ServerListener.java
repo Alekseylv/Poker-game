04e7faf087023497149e05e70e9f016bc58bacad
Alekseylv <alekseylv@me.com>
2013-07-19 11:55:25 +0300

    sdf

--- Poker/src/client/ServerListener.java
+++ Poker/src/client/ServerListener.java
@@ -65,11 +65,7 @@ public class ServerListener implements Runnable {
 				// String token = in.next();
 				// que.addTask(token)
 			} catch (Exception e) {
-				System.out.println();
-				try {
-					in.close();
-				} catch (IOException e1) {
-				}
+				System.out.println("Closed");
 				break;
 			}
 		}
