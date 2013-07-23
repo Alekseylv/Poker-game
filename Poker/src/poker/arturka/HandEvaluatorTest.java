df04d4e28b7e139983e77315c9b0a91fb7f8c48e
Janis Vancans <jaanis.vancaans@gmail.com>
2013-07-23 14:49:43 +0300

    Initialized playerHand list up on creating instance of the PlayerHand class. Commented HandEvaluator class.

--- Poker/src/poker/arturka/HandEvaluatorTest.java
+++ Poker/src/poker/arturka/HandEvaluatorTest.java
@@ -12,12 +12,12 @@ public class HandEvaluatorTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Player p1 = new Player(1);
-		Player p2 = new Player(2);
-		Player p3 = new Player(3);
-		Player p4 = new Player(4);
-		Player p5 = new Player(5);
-		Player p6 = new Player(6);
+		Player p1 = new Player(1, "a");
+		Player p2 = new Player(2, "b");
+		Player p3 = new Player(3, "c");
+		Player p4 = new Player(4, "d");
+		Player p5 = new Player(5, "e");
+		Player p6 = new Player(6, "f");
 
 		Card c1 = new Card(Suit.HEARTS, Rank.JACK);
 		Card c2 = new Card(Suit.SPADES, Rank.JACK);
