b2266f89e3c93efd52b65fa19636c4cf147a1a2e
Alekseylv <alekseylv@me.com>
2013-07-23 14:37:58 +0300

    Fixed client name sending bug

--- Poker/src/client/Client.java
+++ Poker/src/client/Client.java
@@ -33,7 +33,10 @@ public class Client {
 		
 		try {
 			Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
-			(new PrintWriter(socket.getOutputStream())).println(name);
+			PrintWriter out = new PrintWriter(socket.getOutputStream());
+			out.println(name);
+			out.flush();
+			out.close();
 						
 			TaskQueue que = new TaskQueue();
 			Conn conn = new Conn(socket, socket.getOutputStream());
