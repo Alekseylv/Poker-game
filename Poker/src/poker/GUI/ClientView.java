2680d38c8765c9f51dbd44580939bec152678d06
mordavolt <mordavolt@gmail.com>
2013-07-25 10:10:41 +0300

    lalala fixes

--- Poker/src/poker/GUI/ClientView.java
+++ Poker/src/poker/GUI/ClientView.java
@@ -649,7 +649,6 @@ public class ClientView extends JFrame implements ChangeListener, ActionListener
 
         for(SendWinnerListCommand.Tuple player : list){
             if(player != null){
-            	
                 displayBroadcast().setText(displayBroadcast().getText() + "\n" + model.getPlayer(player.id).getNick() + " has won $" + player.cash + " with " + player.hand);
             }
         }
