5678b687ba26ff5292de851e42d03fab02b417b9
Janis Vancans <jaanis.vancaans@gmail.com>
2013-07-25 19:58:55 +0300

    removeUser now removes the specified client output stream from the clientStream list aswell.

--- Poker/src/poker/server/Room.java
+++ Poker/src/poker/server/Room.java
@@ -120,6 +120,7 @@ public class Room implements Runnable {
 		if (connections.containsKey(clientSessionID)
 				&& connections.get(clientSessionID).getClient().isConnected()) {
 			connections.remove(clientSessionID);
+			clientStreams.remove(clientSessionID);
 		}
 	}
 
